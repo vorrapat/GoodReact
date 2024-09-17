9a593038e7e2aea8aad9ae825537e046016109f2
vorrapat <9beamdota@gmail.com>
2024-09-17 23:11:46 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path='/signin' element={<SignIn/>} />
+        <Route exact path='/signup' element={<SignUp/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
